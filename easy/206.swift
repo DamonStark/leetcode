4b44cdf53008af151c29841f963017ac87584107
解炳灿 <jiebingcan@BingCan-MacStudio.local>
2023-08-17 00:19:42 +0800

    206.反转链表

---
+++ easy/206.swift
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func reverseList(_ head: ListNode?) -> ListNode? {
+        var list = head
+        var reverseList: ListNode? = nil
+        while list != nil {
+            var temp = list!.next
+            list!.next = reverseList
+            reverseList = list
+            list = temp
+        }
+        return reverseList
+    }
+}
