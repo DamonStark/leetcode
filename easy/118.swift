02122abb20448a554cbbe43be2b2cbca7731cf70
xiebingcan <xiebingcan@apusapps.com>
2023-10-19 20:42:49 +0800

    118.杨辉三角-非递归

--- easy/118.swift
+++ easy/118.swift
@@ -1,17 +1,15 @@
 class Solution {
     func generate(_ numRows: Int) -> [[Int]] {
-        if numRows == 1 {return [[1]]}
-        if numRows == 2 {return [[1],[1,1]]}
-        var result = generate(numRows - 1)
-        let lastRow = result.last!
-        var row = [1]
-        var i = 1 
-        while i < lastRow.count {
-            row.append(lastRow[i-1] + lastRow[i])
-            i = i + 1
+        var result: [[Int]] = []
+        for i in 1 ... numRows {
+            var row = Array(repeating:1, count: i)
+            if i > 2 {
+                for j in 1 ... (i - 2) {
+                    row[j] = result[i-2][j - 1] + result[i-2][j]
+                }
+            }
+            result.append(row)
         }
-        row.append(1)
-        result.append(row)
         return result
     }
 }
