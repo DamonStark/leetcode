46cbd98b8b513bac2d8fad45f0e78434dec69b32
xiebingcan <xiebingcan@apusapps.com>
2024-03-03 16:14:24 +0800

    225.队列实现栈.双队列

--- easy/225.swift
+++ easy/225.swift
@@ -45,3 +45,36 @@ class MyStack {
         return queue.isEmpty()
     }
 }
+
+class MyStack_TwoQueue {
+
+    var queue: Queue<Int>
+    var toolQueue: Queue<Int>
+
+    init() {
+        queue = Queue<Int>()
+        toolQueue = Queue<Int>()
+    }
+    
+    func push(_ x: Int) {
+        toolQueue.push(x)
+        while !queue.isEmpty() {
+            toolQueue.push(queue.pop()!)
+        }
+        let tmp = queue
+        queue = toolQueue
+        toolQueue = tmp
+    }
+    
+    func pop() -> Int {
+        return queue.pop()!
+    }
+    
+    func top() -> Int {
+        return queue.peek()!
+    }
+    
+    func empty() -> Bool {
+        return queue.isEmpty()
+    }
+}
