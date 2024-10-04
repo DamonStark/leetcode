886130c8182e4a316033e5bcf90b9c376a3d0be4
matt <s3636586400@163.com>
2024-10-04 12:08:36 +0800

    feat: 二叉树反转DFS和BFS迭代解

--- easy/226.swift
+++ easy/226.swift
@@ -13,6 +13,11 @@
  *     }
  * }
  */
+
+
+/**
+* 递归解
+*/
 class Solution {
     func invertTree(_ root: TreeNode?) -> TreeNode? {
         if root == nil {
@@ -20,9 +25,73 @@ class Solution {
         }
         let temp = root!.left
         root!.left = root!.right
-        root!.right = temp
+        root!.right = tempx
         invertTree(root!.left)
         invertTree(root!.right)
+        return root
+    }
+}
+
+/**
+* DFS迭代
+*/
+
+class Stack<T> {
+    var list:[T] = []
+    func push(_ item:T) {
+        list.append(item)
+    }
+    func pop() -> T {
+        return list.removeLast()
+    }
+    func isEmpty() -> Bool {
+        return list.isEmpty
+    }
+}
+class Solution {
+    func invertTree(_ root: TreeNode?) -> TreeNode? {
+        let stack = Stack<TreeNode>()
+        if root != nil {
+            stack.push(root!)
+        }
+        while !stack.isEmpty() {
+            let node = stack.pop()
+            if node.right != nil {
+                stack.push(node.right!)
+            }
+            if node.left != nil {
+                stack.push(node.left!)
+            }
+            let tmp = node.left
+            node.left = node.right
+            node.right = tmp
+        }
+        return root
+    }
+}
+
+
+/**
+* BFS迭代
+*/
+class Solution {
+    func invertTree(_ root: TreeNode?) -> TreeNode? {
+        var queue:[TreeNode] = []
+        if let head = root {
+            queue.append(head)
+            while !queue.isEmpty {
+                let node = queue.removeFirst()
+                if node.left != nil {
+                    queue.append(node.left!)
+                }
+                if node.right != nil {
+                    queue.append(node.right!)
+                }
+                let tmp = node.left
+                node.left = node.right
+                node.right = tmp
+            }
+        }
         return root
     }
 }
